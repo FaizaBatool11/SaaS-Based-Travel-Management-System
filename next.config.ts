5ace2ce16aec13a4a0d59b4acd7ebc2b23e89633
FaizaBatool11 <faizabatool28890@gmail.com>
2025-10-08 08:38:01 -0700

    Expose NEXT_PUBLIC_API_URL in next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,26 @@
+// import type { NextConfig } from "next";
+
+// const nextConfig: NextConfig = {
+//   images: {
+//     remotePatterns: [
+//       {
+//         protocol: "https",
+//         hostname: "img.freepik.com",
+//       },
+//       {
+//         protocol: "https",
+//         hostname: "cdn-icons-png.flaticon.com",
+//       },
+//     ],
+//   },
+// };
+
+// export default nextConfig;
+
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  reactStrictMode: true,
   images: {
     remotePatterns: [
       {
@@ -12,6 +32,9 @@ const nextConfig: NextConfig = {
         hostname: "cdn-icons-png.flaticon.com",
       },
     ],
+  },
+  env: {
+    NEXT_PUBLIC_API_URL: process.env.NEXT_PUBLIC_API_URL,
   },
 };
 
